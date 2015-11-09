bacf6bc15a35079377cfdf70798e352a32b2db76
thuxugang <whuxugang@aliyun.com>
2015-11-09 16:59:50 +0800

    Update qq_count_detail.py

--- qq_count_detail.py
+++ qq_count_detail.py
@@ -16,7 +16,6 @@ from selenium.webdriver.common.by import By
 import time
 import os
 from pyquery import PyQuery as pq
-#import http.client
 import httplib, urllib,urllib2
 import json
 global browser
@@ -29,9 +28,6 @@ def getGroupNames():
     print u"开始.." 
     groupNames = []
     groupId = []
-#    time.sleep(3)
-#    number = browser.find_element_by_xpath("//div[@class='body-content']/div[@class='member-body']/div[@class='my-all-group']")
-#    print number
     browser.switch_to_window(browser.window_handles[-1])
     group = browser.find_elements_by_css_selector(".my-all-group li")
     
@@ -39,37 +35,12 @@ def getGroupNames():
     
     for i in group:
         
-#        print i.text
         groupNames.append(i.text)
         groupId.append(i.get_attribute("data-id"))
     
 
     return groupNames, groupId
 
-#def action(string, groupnames, groupnumbers, last = False):
-#    
-#    webElement = browser.find_element_by_css_selector(string)
-#    
-#    if(last == False):
-#        print webElement.text
-#    
-#    groupnames.append(webElement.text)
-#
-#    ActionChains(browser).double_click(webElement).perform()
-#    indiv = browser.find_element_by_xpath("//div[@class='group-members']")
-#    
-#    pattern = re.compile(r'([0-9]+)/[0-9]+')
-#    number = pattern.findall(indiv.text)[0]
-#    groupnumbers.append(number)
-#    
-#    time.sleep(SLEEP_TIME)
-#    ##  
-#    change = browser.find_element_by_id("changeGroup")
-#    ActionChains(browser).double_click(change).perform()
-#
-#    time.sleep(SLEEP_TIME)
-#    
-#    return groupnames, groupnumbers
     
 def getInfomation(searchName, searchId):
     
@@ -77,17 +48,7 @@ def getInfomation(searchName, searchId):
     browser.switch_to_window(browser.window_handles[-1])
     
     results = []
-#    name = browser.find_element_by_id("groupTit")
-#    
-#    
-##$.post("http://qun.qq.com/cgi-bin/qun_mgr/search_group_members",{gc:46353753,
-##st:0,
-##end:20,
-##sort:0,
-##bkn:744263132},function(a){console.log(a)})    
-#    title = name.text
-#    idnumber = re.compile(r'([0-9]+)/[0-9]+')
-#    print name.text
+
     j = 0
     while(j < len(searchName)):
         
@@ -97,56 +58,25 @@ def getInfomation(searchName, searchId):
         
         
         cookie = browser.get_cookies()
-    #    print cookie
-    #    
-    ##    print cookie
-    #    
-    #    content = {}
     
         skey = ""
         for i in cookie:
             
             name = i.get('name')
-    #        print name
             if(name == "skey"):
                 skey = i.get('value')
                 break
-    #        else:
-    #            skey = ""
-    #            print "wrong"
-    #        
-    #        content[name] = value
-    #        
-    #    string = ""
-    #    names = ['pt_clientip','pt_serverip', 'ptisp', 'RK', 'ptcz', 'pt2gguin', 'uin', 'skey', 'p_uin', 'p_skey', 'pt4_token', 'pgv_info', 'ts_last', 'ts_refer', 'pgv_pvid', 'ts_uid']
-    #js document.cookie    
-    #    for name in names:
-    #        value = content[name]
-    #        
-    #        if (name != names[-1]):
-    #            string = string + name + "=" + value + "; "
-    #        else:
-    #            string = string + name + "=" + value
+
         js = "var string = document.cookie;return string;"        
         string = browser.execute_script(js)
     
     #gtk 算法
-    #    skey = cookie.get('skey')
+
         js2 = "var hash = 5381;  for(var i = 0, len ='" + skey + "'.length; i < len; ++i){ hash += (hash << 5) + '" + skey + "'.charAt(i).charCodeAt();  }  return hash"  
-#        print skey
+
         gtk = browser.execute_script(js2)
         bkn = gtk & 0x7fffffff
         
-#        print string
-#        print bkn
-       
-    #   function getGTK(str){  
-    #var hash = 5381;  
-    #for(var i = 0, len = str.length; i < len; ++i){  
-    #hash += (hash << 5) + str.charAt(i).charCodeAt();  
-    #}  
-    #return hash ;  
-    #}  
         
         url = "http://qun.qq.com/cgi-bin/qun_mgr/search_group_members"
         
@@ -163,68 +93,22 @@ def getInfomation(searchName, searchId):
             'Referer':"http://qun.qq.com/member.html",
             'Host':"qun.qq.com",
             "Cookie":string,
-    #        req.add_header('Cookie',string)
             
             }    
-    #    r = pq(url, params, method='post')
+
         postData = urllib.urlencode(params)
-    #    postHeaders = urllib.urlencode(headers)
-    #    
+
         req=urllib2.Request(url, postData, headers)
-    #    opener = urllib2.build_opener(urllib2.HTTPCookieProcessor()) 
-    #    response = opener.open(req, postData,postHeaders) 
-        
-    #    req.add_header('Cookie',string)
-    #    req.add_header('Content-Type',"application/x-www-form-urlencoded; charset=UTF-8")
-    #    req.add_header('User-Agent',"Mozilla/5.0 (Windows NT 6.1) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/45.0.2454.101 Safari/537.36")
-    #    req.add_header('Referer',"http://qun.qq.com/member.html")
-    #    req.add_header('Accept',"application/json, text/javascript, */*; q=0.01")
-    #    req.add_header('Host',"qun.qq.com")
-    ##    req.add_header('Cookie',string)
-    ##    req.add_header('Cookie',string)        
+      
         resp = urllib2.urlopen(req).read()
         result = json.loads(resp)
-    #    req.set_debuglevel(1)
-    #    res = req.getresponse()
+
     
         
         results.append(result)
         j = j + 1 
         
     return results
-    #    params = urllib.urlencode(params) 
-        
-     
-    
-                
-    #    string = "li[title=" + "'" + i + "'" + "]"
-    #    name = re.sub(' ', '&nbsp;', i) 
-    #    string = "li[title=" + "'" + name + "'" + "]"
-    #            
-    #    webElement = browser.find_element_by_css_selector(string)
-    #    ActionChains(browser).double_click(webElement).perform()
-    #            
-    #    d = pq("td")
-    #    return d
-                
-    #            title = browser.find_element_by_xpath("//td[@class='td-no']")
-    #            indiv = browser.find_element_by_xpath("//td[@class='td-no'][contains(text(),'1')]")
-    #            indiv = browser.find_element_by_css_selector("tbody[class='list']")
-    #            ActionChains(browser).double_click(title).perform()
-    #            title.send_keys(Keys.DOWN)
-                
-    #            print indiv.text
-                
-    
-    
-    #
-    
-    #def getTotalNumbers(groupnumbers):
-    #    total = 0
-    #    for i in groupnumbers:        
-    #        number = int(i)
-    #        total = total + number
-    #    return total
 
 def output(infomations, searchName, searchId):
     file = open("log.txt","w")    
@@ -305,7 +189,7 @@ def output(infomations, searchName, searchId):
     
     file.close()  
     
-#    return sum_group,sum_peple
+
 
         
 if __name__ == '__main__':
@@ -317,10 +201,7 @@ if __name__ == '__main__':
     
     print u"正在打开网页，请稍后.."
 
-#    print u"请给网速打分，good:1-5:bad"
-#    
-#    SLEEP_TIME = raw_input('')
-#    SLEEP_TIME = float(SLEEP_TIME)
+
     
     browser = webdriver.Chrome(chromedriver)
     
@@ -333,7 +214,7 @@ if __name__ == '__main__':
     f = open('.\input.txt','r')  
     for line in open('.\input.txt'):  
         line = f.readline().strip('\n')
-#        print line  
+
         searchName.append(line)
     
     
